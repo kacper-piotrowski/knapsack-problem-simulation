c0e261dc6679621f87062f3c57a6836c643f07de
Kacper <kacper.piotrowski1711@gmail.com>
2026-06-12 01:28:02 +0200

    Add brute force solution

--- main.py
+++ main.py
@@ -15,10 +15,10 @@ def load_file(filename):
                 items.append([item_name, item_weight, item_value])
             return items
     except FileNotFoundError:
-        sys.exit("File not found")
+        sys.exit("Nie znaleziono pliku")
 
 def greedy(items, max_capacity):
-    items.sort(key=lambda item: item[2], reverse=True)
+    items.sort(key=lambda itemlam: itemlam[2]/itemlam[1], reverse=True)
     current_capacity = 0
     current_value = 0
     for item in items:
@@ -27,3 +27,26 @@ def greedy(items, max_capacity):
             current_value += item[2]
             print(f"Włożono {item[0]}, Aktualna waga: {current_capacity}, Aktualna wartość: {current_value}")
 
+def brute_force(items, max_capacity):
+    combos=[[]]
+    for item in items:
+        new_combos = []
+        for combo in combos:
+            new_combo = combo + [item]
+            new_combos.append(new_combo)
+        combos.extend(new_combos)
+
+    max_value = 0
+    max_val_items = []
+    for combo in combos:
+        combo_weight = 0
+        combo_value = 0
+
+        for item in combo:
+            combo_weight += item[1]
+            combo_value += item[2]
+
+        if combo_weight <= max_capacity and combo_value > max_value:
+            max_value = combo_value
+            max_val_items = combo
+    print(f"Najlepsze kombo: {max_val_items}, Wartość: {max_value}")
