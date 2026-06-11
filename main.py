de196f562abe5850f1e400b9d5e58d56a86ad069
Kacper <kacper.piotrowski1711@gmail.com>
2026-06-11 23:56:39 +0200

    Add greedy solution

--- main.py
+++ main.py
@@ -16,3 +16,14 @@ def load_file(filename):
             return items
     except FileNotFoundError:
         sys.exit("File not found")
+
+def greedy(items, max_capacity):
+    items.sort(key=lambda item: item[2], reverse=True)
+    current_capacity = 0
+    current_value = 0
+    for item in items:
+        if current_capacity + item[1] <= max_capacity:
+            current_capacity += item[1]
+            current_value += item[2]
+            print(f"Włożono {item[0]}, Aktualna waga: {current_capacity}, Aktualna wartość: {current_value}")
+
